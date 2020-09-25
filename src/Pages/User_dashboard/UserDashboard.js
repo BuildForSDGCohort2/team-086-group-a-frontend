8fb9aad32ca077d0f3ecb36e9d55621d38960489
hobbyluv <princewillchime43@gmail.com>
2020-09-25 09:00:35 +0100

    added the notification section of the user dash board

--- src/Pages/User_dashboard/UserDashboard.js
+++ src/Pages/User_dashboard/UserDashboard.js
@@ -1,11 +1,15 @@
 import React from "react";
 import DisplayPicture from "../../Components/User_dashboard/DisplayPicture";
 import Notification from "../../Components/User_dashboard/Notification";
+import Header from "../../Common/Header.Component/Header";
+import Footer from "../../Common/Footer.component/Footer";
 const UserDashboard = () => {
   return (
     <div>
+      <Header />
       <DisplayPicture />
       <Notification />
+      <Footer />
     </div>
   );
 };
