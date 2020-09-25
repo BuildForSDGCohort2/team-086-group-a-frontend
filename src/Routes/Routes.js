3050de981f86dd9e808765d56cb054e23357b4b6
hobbyluv <princewillchime43@gmail.com>
2020-09-25 10:27:46 +0100

    added the recommended restaurant section of the user dashboard component

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -7,49 +7,47 @@ import MenuPage from "../Pages/menuPage/MenuPage";
 import UserDashBoard from "../Pages/User_dashboard/UserDashboard";
 import UnProtectedRoutes from "./UnprotectedRoutes";
 import ProtectedRoutes from "./ProtectRouted";
+import { NonRegisteredMemberContextProvider } from "../Context/NonRegisteredMemberContext";
 import NotFoundPage from "../Pages/NotFoundPages/NotFound";
 
 const Routes = () => {
   return (
     <div>
       <Router>
-        <Switch>
-          <UnProtectedRoutes
-            path="/team-086-group-a-frontend/"
-            exact
-            component={LandingPage}
-          />
+        <NonRegisteredMemberContextProvider>
+          <Switch>
+            <UnProtectedRoutes
+              path="/team-086-group-a-frontend/"
+              exact
+              component={LandingPage}
+            />
 
-          <UnProtectedRoutes
-            path="/team-086-group-a-frontend/home"
-            component={LandingPage}
-          />
+            <UnProtectedRoutes
+              path="/team-086-group-a-frontend/home"
+              component={LandingPage}
+            />
 
-          <UnProtectedRoutes
-            path="/team-086-group-a-frontend/signin"
-            component={SigninRegistrationPage}
-          />
+            <UnProtectedRoutes
+              path="/team-086-group-a-frontend/signin"
+              component={SigninRegistrationPage}
+            />
 
-          <UnProtectedRoutes
-            path="/team-086-group-a-frontend/signup"
-            component={SignupRegistrationPage}
-          />
+            <UnProtectedRoutes
+              path="/team-086-group-a-frontend/signup"
+              component={SignupRegistrationPage}
+            />
 
-          <UnProtectedRoutes
-            path="/team-086-group-a-frontend/signin"
-            component={SigninRegistrationPage}
-          />
-
-          <ProtectedRoutes
-            path="/team-086-group-a-frontend/menu"
-            component={MenuPage}
-          />
-          <ProtectedRoutes
-            path="/team-086-group-a-frontend/dashboard"
-            component={UserDashBoard}
-          />
-          <Route render={NotFoundPage} />
-        </Switch>
+            <ProtectedRoutes
+              path="/team-086-group-a-frontend/menu"
+              component={MenuPage}
+            />
+            <ProtectedRoutes
+              path="/team-086-group-a-frontend/dashboard"
+              component={UserDashBoard}
+            />
+            <Route render={NotFoundPage} />
+          </Switch>
+        </NonRegisteredMemberContextProvider>
       </Router>
     </div>
   );
