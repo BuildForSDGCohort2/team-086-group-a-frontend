e27a0be780ff4dc1f32cba59fd8ede914182f6a6
hobbyluv <princewillchime43@gmail.com>
2020-10-02 20:10:20 +0100

    added the topArea view of the blogs page

--- src/Pages/BlogPage/BlogPage.js
+++ src/Pages/BlogPage/BlogPage.js
@@ -2,17 +2,18 @@ import React, { useContext } from "react";
 import Header from "../../Common/Header.Component/Header";
 import Footer from "../../Common/Footer.component/Footer";
 import { RegisterContextMembers } from "../../Context/RegisteredMemberContext";
+import TopBgImage from "../../Components/Blogs/TopBgImage";
 
 const BlogPage = () => {
   const [state] = useContext(RegisterContextMembers);
-  console.log("state", state);
   const { headerNavLinksRegCollections } = state;
   return (
     <div>
       {sessionStorage.getItem("Token") ? (
         <Header headerNavLinks={headerNavLinksRegCollections} />
       ) : null}
-      <h2>hellow</h2>
+
+      <TopBgImage />
 
       <Footer />
     </div>
